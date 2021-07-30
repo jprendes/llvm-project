46b4bb65b8b2facf2728824f050315a9d238adb9
Luís Marques <luismarques@lowrisc.org>
2021-07-30 11:25:16 +0100

    [Clang] Add Clang driver support for ePIC

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -1117,6 +1117,8 @@ static const char *RelocationModelName(llvm::Reloc::Model Model) {
     return "pic";
   case llvm::Reloc::DynamicNoPIC:
     return "dynamic-no-pic";
+  case llvm::Reloc::EPIC:
+    return "epic";
   case llvm::Reloc::ROPI:
     return "ropi";
   case llvm::Reloc::RWPI:
